fc4706d9778794b8ecba70660204e78f504ffa59
Davide Canton <davide.canton5@gmail.com>
2019-01-12 00:15:01 +0100

    small refactoring

--- pytco.py
+++ pytco.py
@@ -10,7 +10,7 @@ def bind_params(names, call_node):
     values = {}
 
     if len(names) != len(call_node.args):
-        raise ValueError(f"Incorrect number of params ${len(call_node.args)} (expected ${len(names)})")
+        raise ValueError(f"Incorrect number of params {len(call_node.args)} (expected {len(names)})")
 
     for name, arg in zip(names, call_node.args):
         values[name] = arg
@@ -59,8 +59,8 @@ def tco(func):
     TCO_Replace(func, t).replace_nodes()
     ast.fix_missing_locations(t)
     src = astunparse.unparse(t)
-    fmt = "{}\nret={}(*args, **kwargs)"
-    code = compile(fmt.format(src, func.__name__), "<string>", "exec")
+    fmt = f"{src}\nret={func.__name__}(*args, **kwargs)"
+    code = compile(fmt, "<string>", "exec")
 
     def w(*args, **kwargs):
         exec(code, globals(), locals())
